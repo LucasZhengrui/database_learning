2812fdd77375ceb62617a6672d3c3a3f4fb66f7f
LucasZhengrui <lucaszhengrui@gmail.com>
2019-10-29 19:26:20 +0800

    三表展示

---
+++ university_cn库三表展示.sql
@@ -0,0 +1,32 @@
+select *
+from student;
+
+--stu_id	stu_name	stu_sex	stu_age	stu_dept
+--200215121	李勇                	男	20	CS                  
+--200215122	刘晨                	女	19	CS                  
+--200215123	王敏                	女	18	MA                  
+--200215125	张立                	男	19	IS                  
+
+select *
+from elective_course;
+
+--stu_id	course_id	grade
+--200215121	1   	92
+--200215121	2   	85
+--200215121	3   	88
+--200215122	2   	90
+--200215122	3   	80
+--200215123	2   	80
+
+select *
+from course;
+
+--course_id	course_name	course_pro_id	course_credit
+--1   	数据库                                  	5   	4
+--2   	数学                                    	NULL	2
+--3   	信息系统                                	1   	4
+--4   	操作系统                                	6   	3
+--5   	数据结构                                	7   	4
+--6   	数据处理                                	NULL	2
+--7   	PASCAL语言                              	6   	4
+

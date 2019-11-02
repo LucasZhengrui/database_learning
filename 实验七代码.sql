058d0f750e8f9125d8a3fa89d2d13e0948905dbc
LucasZhengrui <lucaszhengrui@gmail.com>
2019-11-02 12:16:42 +0800

    实验七code

--- 实验七代码.sql
+++ 实验七代码.sql
@@ -1,4 +1,4 @@
--- 1．	创建角色ProgramerRole，拥有创建表，存储过程，视图权限，拥有对Salary表的查询、修改、插入权限
+-- 1.创建角色ProgramerRole，拥有创建表，存储过程，视图权限，拥有对Salary表的查询、修改、插入权限
 
 -- 创建角色ProgramerRole
 create role ProgramerRole
@@ -8,12 +8,19 @@ grant select,insert,alter
 on Salary
 to ProgramerRole
 
--- 授予创建表，存储过程，视图权限
+-- 授予创建表，存储过程
+grant create table,execute 
+to ProgramerRole
 
--- 2．	创建一个登录账号Testlogin
+-- 授予视图权限
+grant create view 
+to ProgramerRole
 
+-- 2.创建一个登录账号Testlogin
 create login Testlogin with password ='123456'
-create user testuser from Testlogin
 
--- 3．	创建对应于这个登录账号的数据库用户TestUser
--- 4．	将用户TestUser添加到TestRole角色中
+-- 3.创建对应于这个登录账号的数据库用户TestUser
+create user TestUser from login Testlogin;
+
+-- 4.将用户TestUser添加到TestRole角色中
+exec sp_addrolemember 'ProgramerRole','TestUser'
